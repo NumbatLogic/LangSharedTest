23414f0a22f30909b5ab3140140ffcd59dc03570
CliffyA <github@cliffya.com>
2026-03-23 00:46:34 +1100

    Build fixes

--- Transpile.sh
+++ Transpile.sh
@@ -4,7 +4,7 @@ mapfile -t sPackages < LangShared.package-list
 sLangSharedArgs=()
 for sPackage in "${sPackages[@]}"; do
 	sLangSharedArgs+=("-f")
-	sLangSharedArgs+=("../LangShared/$sPackage")
+	sLangSharedArgs+=("../LangShared/Source/$sPackage")
 done
 
 gdb -ex "set confirm off" -ex run -ex backtrace -ex quit --args ../Lang/Bin/CLI -f Source "${sLangSharedArgs[@]}"

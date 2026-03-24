c08468adeaf553a83092a9626805db585015e59f
CliffyA <github@cliffya.com>
2026-03-24 20:37:54 +1100

    Don't need dis

--- Project/LangSharedTest/CPP/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../../Transpiled/LangSharedTest/LangSharedTest.hpp"
-#include <cstdio>
-
-int main(int argc, const char* argv[])
-{
-	NumbatLogic::LangSharedTest::Run();
-	return 0;
-}
--- Project/LangSharedTest/CS/Main.cs
+++
@@ -1,7 +0,0 @@
-class Application
-{
-	static void Main(string[] args)
-	{
-		NumbatLogic.LangSharedTest.Run();
-	}
-}
